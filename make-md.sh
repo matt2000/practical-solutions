d1e726f547603f7d962644170058f42c26d4fdc8
Matt Chapman <Matt@NinjitsuWeb.com>
2014-08-26 15:25:48 -0700

    More complete Literate BASH example.

--- make-md.sh
+++ make-md.sh
@@ -9,7 +9,8 @@
 # Literate BASH Requirements
 # --------------------------
 #
-# We require bash scripts that are intended to be converted to start as follows:
+# We require bash scripts that are intended to be converted to be formatted
+# roughly as follows, with the first 10 lines being the essential part:
 #
 #     #!/bin/bash
 #
@@ -17,19 +18,37 @@
 #     # =================================
 #     # space-separated tags go here
 #     #
+#     # Descriptive excerpt goes here. These end up on the Jekyll home page.
+#     #
 #     # First subheading marks end of excerpt
 #     # -------------------------------------
 #
-# And now, teh codez.
-# -------------------
+#     # We might put some code we actually want executed here...
+#
+#     echo "Hello World!"
+#
+#     # We can even put code in the comments in our code:
+#     #
+#     #    echo "Infinite recursion happens here. Except it doesn't. Or does it?"
+#     #
+#     # Scary, huh?
+#
+#     exit 1
+#
+# And now, teh codez that make that^^ into Markdown that Jekyll can make into HTML.
+# -----------------------------------------
 
-# Get the date.
+# Get the date, from the command-line, or from today.
 
-today=`date +%Y-%m-%d`
+day="$2"
+if [[ -z $day ]]
+then
+  day=`date +%Y-%m-%d`
+fi
 
 # Store to the Jekyll posts folder.
 
-outfile="_posts/$today-$1.md"
+outfile="_posts/$day-$1.md"
 
 # Strip the hash-bang.
 
@@ -44,7 +63,8 @@ sed -i 's/\(^.*$\)/    \1/' $outfile
 sed -i 's/^    #$//' $outfile
 sed -i 's/^    # \(.*$\)/\1/' $outfile
 
-# Add Jekyll front-matter.
+# Add Jekyll front-matter. If we just wanted Markdown and not Jekyll posts, we
+# could exit here.
 
 sed -i "1s;^;---\nlayout: post\ntitle: ;" $outfile
 
